1c831a8d6061d560ee0ff1bb32b27f8038112eb4
Patrice Vanpaemel <pvantmails@gmail.com>
2023-07-11 15:01:01 +0200

    Added an input check

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+def check_iban(iban):
+    if not iban:
+        print('IBAN is none. Please type something')
+        return False
+    elif len(iban) > 34:
+        print('IBAN is too long. An IBAN has a maximum length of 34 characters.')
+        return False
+    elif iban[0].isdigit() or iban[1].isdigit():
+        print('IBAN is invalid. The first two characters in an IBAN need to be alphabetical characters.')
+        return False
+    else:
+        return True
+
+
 def get_fours(iban_list):
     set_of_fours = ''
     list_length = len(iban_list)
@@ -8,7 +22,14 @@ def get_fours(iban_list):
     return set_of_fours
 
 
-iban = input()
+valid = False
+
+while not valid:
+    iban = check_iban(input())
+
+
+if len(iban) > 34:
+    print('IBAN is too long. An IBAN has a maximum length of 34 characters.')
 
 iban_list = list(iban)
 split_iban = ''
